93d368515ef0680a7d75ecd82531a37fb678a8f1
Solar M. <me@benjmainmacaulay.com>
2025-07-03 12:58:17 +0100

    refactor: moved framework to its own folder

--- MongoDbEntityFramework/Repository/EntityRepository.cs
+++ MongoDbEntityFramework/Repository/EntityRepository.cs
@@ -27,6 +27,11 @@ public class EntityRepository<TEntity> : IEntityRepository<TEntity>
         _collection = database.GetCollection<TEntity>(typeof(TEntity).Name);
     }
 
+    /// <summary>
+    /// Inserts a new entity into the MongoDB collection.
+    /// </summary>
+    /// <param name="entity">The entity to insert.</param>
+    /// <returns>True if the insert operation completed; otherwise, false.</returns>
     public bool Insert(TEntity entity)
     {
         entity.Id = ObjectId.GenerateNewId();
@@ -35,6 +40,11 @@ public class EntityRepository<TEntity> : IEntityRepository<TEntity>
         return task.IsCompleted;
     }
 
+    /// <summary>
+    /// Updates an existing entity in the collection or inserts it if it does not exist.
+    /// </summary>
+    /// <param name="entity">The entity to update or insert.</param>
+    /// <returns>True if the entity was updated; otherwise, false.</returns>
     public bool Update(TEntity entity)
     {
         if (entity.Id == ObjectId.Empty)
@@ -44,26 +54,49 @@ public class EntityRepository<TEntity> : IEntityRepository<TEntity>
             .ModifiedCount > 0;
     }
 
+    /// <summary>
+    /// Updates an existing entity in the collection or inserts it if it does not exist.
+    /// </summary>
+    /// <param name="entity">The entity to update or insert.</param>
+    /// <returns>True if the entity was updated; otherwise, false.</returns>
     public bool Delete(TEntity entity)
     {
         return _collection.DeleteOne(x => x.Id == entity.Id).DeletedCount > 0;
     }
 
+    /// <summary>
+    /// Gets the total number of documents in the collection.
+    /// </summary>
+    /// <returns>The count of documents.</returns>
     public long Count()
     {
         return _collection.CountDocuments(new BsonDocument());
     }
 
+    /// <summary>
+    /// Retrieves all entities from the collection.
+    /// </summary>
+    /// <returns>A list of all entities.</returns>
     public List<TEntity> GetAll()
     {
         return _collection.AsQueryable().ToList();
     }
 
+    /// <summary>
+    /// Searches for entities matching the specified expression.
+    /// </summary>
+    /// <param name="expression">The filter expression.</param>
+    /// <returns>A list of matching entities.</returns>
     public IList<TEntity> SearchFor(Expression<Func<TEntity, bool>> expression)
     {
         return _collection.AsQueryable().Where(expression).ToList();
     }
 
+    /// <summary>
+    /// Retrieves a single entity by its ObjectId.
+    /// </summary>
+    /// <param name="id">The ObjectId of the entity.</param>
+    /// <returns>The entity if found; otherwise, null.</returns>
     public async Task<TEntity?> GetSingle(ObjectId id)
     {
         IAsyncCursor<TEntity>? obj = await _collection.FindAsync(x => x.Id == id);
